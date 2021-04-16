bbd39edbd049282f484edc5a61c9858e14d65a4a
JoelTapia99 <joelts159@gmail.com>
2021-04-15 20:11:42 -0500

    arreglo de fix

--- app/Models/JobBoard/Company.php
+++ app/Models/JobBoard/Company.php
@@ -2,27 +2,18 @@
 
 namespace App\Models\JobBoard;
 
-<<<<<<< HEAD
-use App\Traits\StateActiveTrait;
-=======
->>>>>>> 33d8fa6204c5bd9235b88ab1a6eefe2fb7bf288a
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
 use App\Models\Authentication\User;
-<<<<<<< HEAD
-=======
 use App\Models\App\Catalogue;
->>>>>>> 33d8fa6204c5bd9235b88ab1a6eefe2fb7bf288a
+
 
 class Company extends Model implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
     use HasFactory;
-<<<<<<< HEAD
-    use StateActiveTrait;
-=======
->>>>>>> 33d8fa6204c5bd9235b88ab1a6eefe2fb7bf288a
 
     protected $connection = 'pgsql-job-board';
     protected $table = 'job_board.companies';
@@ -31,24 +22,14 @@ class Company extends Model implements Auditable
     protected $fillable = [
         'trade_name',
         'comercial_activity',
-<<<<<<< HEAD
-        'web_page'
-=======
         'web',
->>>>>>> 33d8fa6204c5bd9235b88ab1a6eefe2fb7bf288a
     ];
-
-
 
     public function offers()
     {
         return $this->hasMany(Offer::class);
     }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 33d8fa6204c5bd9235b88ab1a6eefe2fb7bf288a
     public function user()
     {
         return $this->belongsTo(User::class);
