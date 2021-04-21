ef330685bc8331eb0ab6f2f2ca6c32db06b1227c
cmtl0204 <cesar.tamayo0204@gmail.com>
2021-04-21 16:37:42 -0500

    init

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,27 +12,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AuthenticationSeeder::class,
-            JobBoardSeeder::class,
         ]);
     }
 }
-/*
-                drop schema if exists public cascade;
-                drop schema if exists authentication cascade;
-                drop schema if exists attendance cascade;
-                drop schema if exists app cascade;
-                drop schema if exists job_board cascade;
-                drop schema if exists web cascade;
-                drop schema if exists teacher_eval cascade;
-                drop schema if exists community cascade;
-                    drop schema if exists cecy cascade;
-
-            create schema authentication;
-            create schema attendance;
-            create schema app;
-            create schema job_board;
-            create schema web;
-            create schema teacher_eval;
-            create schema community;
-            create schema cecy;
- */
